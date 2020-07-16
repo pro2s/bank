87d39f694bfeb78fc2e40e3cf7436b621cb7a5c1
Sergey Protasov <s.protasov@itransition.com>
2020-07-16 18:17:36 +0300

    Add ngrok support

--- src/server.js
+++ src/server.js
@@ -2,21 +2,31 @@
 
 const app = require('express')();
 const url = require('url');
+const ngrok = require('./lib/ngrok');
 
 const port = process.env.PRORT || 3000;
 const host = process.env.HOST || 'http://www.client.example.com';
 const callback = '/callback';
-const callbackUrl = url.parse(`${host}:${port}${callback}`);
-
-module.exports = (cb) => {
+const baseUrl = url.parse(`${host}:${port}${callback}`);
+const listener = (callbackUrl, cb) => {
   app.listen(port, (err) => {
     if (err) return console.error(err);
-
-    console.log(`Express server listening at ${callbackUrl.href}, site ${url.resolve(callbackUrl, '/')}`);
-
+    console.log('Set the new webhook to"', callbackUrl, '" server: ', url.resolve(callbackUrl, '/'));
     return cb({
       app,
-      callbackUrl: callbackUrl.href,
+      callbackUrl,
     });
+  });
+};
+
+module.exports = (cb) => {
+  return ngrok.getPublicUrl().then(publicUrl => {
+    console.log('Set the new webhook to"', publicUrl);
+
+    listener(url.resolve(publicUrl, callback), cb);
+  }).catch(error => {
+    console.log('Can not connect to ngrok server. Is it running?');
+
+    listener(baseUrl, cb);
   });
 };
