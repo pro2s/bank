e1c64aa1e61bc1ac9af310b65c039ae704db0e03
Sergey Protasov <protasov.s.s@gmail.com>
2020-11-05 22:18:12 +0300

    Fix port

--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
 'use strict';
 const express = require('express');
 const bodyParser = require('body-parser');
-const session = require('express-session')
+const session = require('express-session');
 const url = require('url');
 const ngrok = require('./lib/ngrok');
 
 const app = express();
 app.use(express.static('public'));
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.set('trust proxy', 1);
@@ -18,11 +18,11 @@ app.use(session({
   saveUninitialized: true,
 }));
 
-const port = process.env.PRORT || 3000;
+const port = process.env.PORT || 3000;
 
 const getBaseUrl = (port) => {
-  if (process.env.HEROKU_URL) {
-    return process.env.HEROKU_URL;
+  if (process.env.PRODUCTION_URL) {
+    return process.env.PRODUCTION_URL;
   } else {
     const host = process.env.HOST || 'http://www.client.example.com';
     return url.parse(`${host}:${port}`);
@@ -33,7 +33,7 @@ const baseUrl = getBaseUrl(port);
 
 const listener = (appUrl, init) => app.listen(port, (err) => {
   if (err) return console.error(err);
-  console.log('Set the new webhook to"', appUrl, '" server: ', url.resolve(appUrl, '/'));
+  console.log('Start server: ', url.resolve(appUrl, '/'));
 
   return init(app, appUrl);
 });
@@ -41,7 +41,7 @@ const listener = (appUrl, init) => app.listen(port, (err) => {
 module.exports = async (init) => {
   try {
     const publicUrl = await ngrok.getPublicUrl();
-    console.log('Set the new webhook to"', publicUrl);
+    console.log('Start ngrok on ', publicUrl);
 
     listener(publicUrl, init);
   } catch (error) {
